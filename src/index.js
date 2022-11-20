d45f23e967de11f702050642e923b0e70d5bfcde
Mehdi Rezaii <99mehdirezaii99@gmail.com>
2022-11-20 10:58:54 +0330

    test

--- src/index.js
+++ src/index.js
@@ -27,38 +27,39 @@ import BlogDetails from './components/blog-details';
 
 
 class Root extends Component {
-    render() {
-        return(
-                <Router >
-	                <Switch>
-	                    <Route exact path="/" component={HomeV1} />
-	                    <Route path="/home-v2" component={HomeV2} />
-	                    <Route path="/home-v3" component={HomeV3} />
-	                    <Route path="/about" component={AboutUs} />
-	                    <Route path="/tour-list" component={TourList} />
-	                    <Route path="/tour-list-v2" component={TourListV2} />
-	                    <Route path="/tour-list-v3" component={TourListV3} />
-	                    <Route path="/tour-details" component={TourDetails} />
-	                    <Route path="/destination-list" component={DestinationLIst} />
-	                    <Route path="/destination-list-v2" component={DestinationLIstV2} />
-	                    <Route path="/destination-details" component={DestinationDetails} />
-	                    <Route path="/gallery" component={Gallery} />
-	                    <Route path="/gallery-details" component={GalleryDetails} />
-	                    <Route path="/faq" component={Faq} />
-	                    <Route path="/contact" component={Contact} />
-	                    <Route path="/error" component={Error} />
-	                    <Route path="/comming-soon" component={CommingSoon} />
-	                    <Route path="/user-profile" component={UserProfile} />
-	                    <Route path="/blog" component={Blog} />
-	                    <Route path="/blog-v2" component={BlogV2} />
-	                    <Route path="/blog-v3" component={BlogV3} />
-	                    <Route path="/blog-details" component={BlogDetails} />
-	                </Switch>
-                </Router>
-        )
-    }
+	render() {
+		return (
+			<HashRouter basename="/">
+					<Switch>
+						<Route exact path="/" component={HomeV1} />
+						<Route path="/home-v2" component={HomeV2} />
+						<Route path="/home-v3" component={HomeV3} />
+						<Route path="/about" component={AboutUs} />
+						<Route path="/tour-list" component={TourList} />
+						<Route path="/tour-list-v2" component={TourListV2} />
+						<Route path="/tour-list-v3" component={TourListV3} />
+						<Route path="/tour-details" component={TourDetails} />
+						<Route path="/destination-list" component={DestinationLIst} />
+						<Route path="/destination-list-v2" component={DestinationLIstV2} />
+						<Route path="/destination-details" component={DestinationDetails} />
+						<Route path="/gallery" component={Gallery} />
+						<Route path="/gallery-details" component={GalleryDetails} />
+						<Route path="/faq" component={Faq} />
+						<Route path="/contact" component={Contact} />
+						<Route path="/error" component={Error} />
+						<Route path="/comming-soon" component={CommingSoon} />
+						<Route path="/user-profile" component={UserProfile} />
+						<Route path="/blog" component={Blog} />
+						<Route path="/blog-v2" component={BlogV2} />
+						<Route path="/blog-v3" component={BlogV3} />
+						<Route path="/blog-details" component={BlogDetails} />
+					</Switch>
+			</HashRouter>
+			
+		)
+	}
 }
 
 export default Root;
 
-ReactDOM.render('hello', document.getElementById('viaje'));
+ReactDOM.render(<Root />, document.getElementById('viaje'));
